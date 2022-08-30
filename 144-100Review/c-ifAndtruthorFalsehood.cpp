eedae0308c9ad870c9a7d979e27bb8f80541d0ca
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2022-08-30 16:01:34 -0400

    Update c-ifAndtruthorFalsehood.cpp

--- 144-100Review/c-ifAndtruthorFalsehood.cpp
+++ 144-100Review/c-ifAndtruthorFalsehood.cpp
@@ -1,6 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 using namespace std;
+
+/*
+"It is important to consider when writing a program:
+ -> Decision making: which means that based on a certain condition you do something;
+ -> And based on certain condition, you repeat something!
+ Based on some decision: do something; or based on some decision: repeat something!"
+ A decision is to treat the outcome of an operation as true or false (truth or falsehood)! Then, from here act in different ways according to what it's needed.
+ 
+ The simplest decision is using if (condition)
+ The condition can be any operation that returns a value (true or false)
+*/
+
 int foo() {
    int d;
    printf("enter a number: ");
