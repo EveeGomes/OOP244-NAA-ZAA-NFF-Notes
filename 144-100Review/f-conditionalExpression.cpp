fff8cdd98608f64ab8d175bb8b440754772bbe8f
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2022-08-30 16:17:37 -0400

    Update f-conditionalExpression.cpp

--- 144-100Review/f-conditionalExpression.cpp
+++ 144-100Review/f-conditionalExpression.cpp
@@ -8,15 +8,16 @@ int main() {
    printf("Enter two integers: ");
    scanf("%d %d", &a, &b);
    // fast
-   max = a > b ? a : b; //  (a and b must be of same type)
+   max = a > b ? a : b; //  (a and b must be of same type or convertable) 
+                        // -> it's wrong to have something like max = a > b ? 12 : 23.4; since 12 and 23.4 aren't of the same type!
    // slow
- /*  if (a > b) {
+   /*  if (a > b) {
       max = a;
    }
    else {
       max = b;
-   }*/
-
+   }
+   */
    printf("%d is bigger!\n", max);
 
    return 0;
