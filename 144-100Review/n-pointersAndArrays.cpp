80cfcf1985925393dd687723ddce0bfbf91b88c6
Eveline Gomes <52388193+EveeGomes@users.noreply.github.com>
2022-09-02 09:19:29 -0400

    Update n-pointersAndArrays.cpp

--- 144-100Review/n-pointersAndArrays.cpp
+++ 144-100Review/n-pointersAndArrays.cpp
@@ -9,9 +9,11 @@ int main() {
    *ar = 123;
    for (i = 0; i < 5; printf("%d ", ar[i++]));
    printf("\n");
-   ar[3] = 3333;      // *(ar + 3)
+   ar[3] = 3333;      // *(ar + 3) -> add 3 integers to the address of ar 
    *(ar + 1) = 1111; // ar[1]
    for (i = 0; i < 5; printf("%d ", ar[i++]));
 
    return 0;
 }
+
+// when int ar[5] is created, the C language creates a const int* ar (a pointer) in another addess that will hold the address of the first element of ar[] (ar[0]).
